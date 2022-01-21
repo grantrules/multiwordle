8da7876d68e7fdeb55b6e45fe58cf5aea1f189c0
Grant <gharding@gmail.com>
2022-01-21 02:36:21 -0500

    maxClients state

--- server/src/rooms/schema/State.js
+++ server/src/rooms/schema/State.js
@@ -2,17 +2,19 @@ import * as schema from '@colyseus/schema';
 import Player from './Player.js';
 
 export default class State extends schema.Schema {
-  constructor() {
+  constructor(maxClients) {
     super();
     this.wordLength = 5;
     this.word = '';
     this.players = new schema.MapSchema();
-    this.gameState = false;
+    this.gameState = 'lobby';
+    this.maxClients = maxClients;
   }
 }
 
 schema.defineTypes(State, {
   players: { map: Player },
+  maxClients: 'number',
   wordLength: 'number',
   word: 'string',
   gameState: 'string',
