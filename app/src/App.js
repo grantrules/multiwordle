f26fe462e61f4ff3833d787bf57c34d801a4ab73
Grant <gharding@gmail.com>
2022-01-21 12:04:08 -0500

    randomized starting name

--- app/src/App.js
+++ app/src/App.js
@@ -3,13 +3,16 @@ import Game from './components/Game';
 import Ctx from './Ctx';
 import Store from './store/Store';
 
+const names = ['Stephen King', 'Ayn Rand', 'Michael Chrichton', 'Anne Rice', 'Mark Twain', 'Voltaire', 'J.K. Rowling'];
+const pickName = () => names[Math.floor(Math.random() * names.length)];
+
 function App() {
   const [store] = React.useState(() => {
     const s = new Store();
 
     // player context
     const storedData = JSON.parse(localStorage.getItem('wordle') || '{}');
-    s.set('playerName', storedData.name || 'Stephen King');
+    s.set('playerName', storedData.name || pickName());
 
     // game context
     s.set('gameClient', null);
